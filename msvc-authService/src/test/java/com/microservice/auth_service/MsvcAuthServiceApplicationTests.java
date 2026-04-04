e930c5e04615fbca14e2108e3f6ff7a96d6b3b4b
Alcindo Chavarria <139635361+ByOneChav@users.noreply.github.com>
2026-04-04 16:33:15 -0300

    crear microservicio authService

---
+++ msvc-authService/src/test/java/com/microservice/auth_service/MsvcAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
