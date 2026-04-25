37d995be4d5869ad9992446cde5fdf3734fcfd8d
Alcindo Chavarria <139635361+ByOneChav@users.noreply.github.com>
2026-04-25 17:22:11 -0400

    sincronizado msvc-inventario con el frontend

--- frontend/src/app/features/inventario/inventario-routing-module.ts
+++ frontend/src/app/features/inventario/inventario-routing-module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { InventarioComponent } from './inventario';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class InventarioRoutingModule {}
+export const INVENTARIO_ROUTES: Routes = [
+  {
+    path: '',
+    component: InventarioComponent
+  }
+];
